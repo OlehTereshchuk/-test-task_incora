788227dca58b622bf51f74d81aa93316e45f8734
OlehTereshchuk <olehtereshchuk@ukr.net>
2020-01-28 21:26:21 +0200

    Updates

---
+++ precache-manifest.352cb5c83e73ec52bb8808ed14cc09ce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "648f0f3fa23667abe31f1d17fe11426a",
+    "url": "/-test-task_incora/index.html"
+  },
+  {
+    "revision": "480017af3f41571ca1c0",
+    "url": "/-test-task_incora/static/css/main.26b56085.chunk.css"
+  },
+  {
+    "revision": "c1a4db1ba054e582ae2b",
+    "url": "/-test-task_incora/static/js/2.8a90629e.chunk.js"
+  },
+  {
+    "revision": "811b3d2d36f5d93ac3b9bf1c6d65ac21",
+    "url": "/-test-task_incora/static/js/2.8a90629e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "480017af3f41571ca1c0",
+    "url": "/-test-task_incora/static/js/main.cbea2e67.chunk.js"
+  },
+  {
+    "revision": "5e22c341992bee03f95a",
+    "url": "/-test-task_incora/static/js/runtime-main.e3e5f3c1.js"
+  }
+]);
